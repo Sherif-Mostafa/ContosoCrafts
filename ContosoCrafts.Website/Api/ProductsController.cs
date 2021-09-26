3def2e76d9f744ea0838dd87bcfa38919130d92f
Sherif Mostafa <sherif.mohamed3@vodafone.com>
2021-09-26 22:40:19 +0200

    Add Product Api

---
+++ ContosoCrafts.Website/Api/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using ContosoCrafts.Website.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.Website.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        public JsonFileProductService _ProductService { get; }
+        public ProductsController(JsonFileProductService productService)
+        {
+            _ProductService = productService;
+        }
+        [Route("{id}")]
+        [HttpGet]
+        public Product Get(string id)
+        {
+            var data = _ProductService.GetProducts().First(product => product.Id == id);
+            return data ?? null;
+        }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _ProductService.GetProducts();
+        }
+
+
+    }
+}
